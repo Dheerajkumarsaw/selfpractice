35e95dabf965fc26c0749c6c6df9730808f21667
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-04-08 23:47:44 +0530

    hi this day 8 apr

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,17 +8,16 @@ router.get('/test-me', function (req, res) {
     console.log('The endpoint value is', logger.endpoint)
     console.log('Calling log function')
     logger.logging()
-    res.send('My first ever api!')
 });
 
 router.get('/test-me2', function (req, res) {
     console.log('I am inside the second route handler')
-    res.send('My second ever api!')
+    res.send('My second sghjdfghjdfghjdfghjfghever api!')
 });
 
 
 router.get('/test-me5', function (req, res) {
-    res.send('My final ever api!')
+    res.send({msg:'My final ever json api!'})
 });
 
 router.get('/test-me3', function (req, res) {
@@ -28,6 +27,24 @@ router.get('/test-me3', function (req, res) {
 router.get('/test-me4', function (req, res) {
     res.send('My first ever api!')
 });
+router.get('/missingno/:a/:b',function(req,res){
+    let new1 = req.params.a;
+    let new2= req.params.b;
+    let ab=0;
+    let sum =0
+    let miss
+        for (let i=new1;i=new2;i++){
+            ab+=i
+        let num =[5,7,8,9,10,11,12]
+        for(let i=0;i<num.length;i++){
+            sum =+ num[i]
+            // let n;
+             miss=ab-sum;
+        }
+    }
+    res.send(miss)
+
+    });
 
 module.exports = router;
 // adding this comment for no reason
